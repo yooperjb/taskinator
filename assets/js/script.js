05ddbf26e783a059faf64eaf22f406169cdddc92
yooperjb <jmb181@humboldt.edu>
2020-10-05 18:47:04 -0700

    create taskhandler function with button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 var buttonEl = document.querySelector("#save-task");
 console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = 'task-item';
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
+
